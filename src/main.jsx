483afc0ff4c062bbcaac76ea28c7d0776f507ab1
Chanakanit <177916223+Chanakanit@users.noreply.github.com>
2026-02-24 23:21:25 +0700

    delete App.css & index.css, modify main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ChakraProvider>
-      <ColorModeScript />
       <App />
     </ChakraProvider>
   </React.StrictMode>
